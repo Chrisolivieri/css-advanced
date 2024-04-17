9c205ad0988a30990fc5d080c02714cc4fe806c0
Chrisolivieri <christian.olivieri93@gmail.com>
2024-04-17 17:51:27 +0200

    update

---
+++ javascript.js
@@ -0,0 +1,14 @@
+let twitter = document.querySelector(".p-4:nth-of-type(3) li:nth-child(2) a")
+
+twitter.style.display = "none"
+
+let contenuto = document.querySelector(".col p")
+let contenuto2 = document.querySelector(".lead")
+
+function cancellaContenuto(){
+    contenuto.innerHTML = " "
+    contenuto2.innerHTML = " "
+}
+ 
+
+
